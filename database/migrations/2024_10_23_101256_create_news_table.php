e06858e412b5349bf1edaaf343deeff3da796e48
TelmanBayramov <telmanbayramov450@gmail.com>
2024-10-23 14:21:28 +0400

    hello

---
+++ database/migrations/2024_10_23_101256_create_news_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',160);
+            $table->string('slug',255)->unique();
+            $table->text('content');
+            $table->string('image',255)->nullable();
+            $table->foreignId('category_id')->constrained('categories')->onDelete(('cascade'));
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
